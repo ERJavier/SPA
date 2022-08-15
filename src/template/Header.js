ba7ec68a53c67289e5125c87fa6cca8caf28ddd5
Javier Cepeda <erjavierc@gmail.com>
2022-08-15 17:16:11 -0400

    home

---
+++ src/template/Header.js
@@ -0,0 +1,20 @@
+const Header = () => {
+    const view = `
+        <div class="Header">
+            <div class="Heder-logo">
+                <h1>
+                    <a href="/">
+                        100tifi.co
+                    </a>
+                </h1>
+            </div>
+        </div>
+        <div class="Header-nav">
+            <a href="#/about/"></a>
+                About
+        </div>
+    `;
+    return view;
+};
+
+export default Header;
